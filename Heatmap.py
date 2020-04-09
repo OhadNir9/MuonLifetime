b05a818e0e301a4ed4ca106f6a81749b18ecd1be
OhadNir9 <57434766+ohadnir9@users.noreply.github.com>
2020-04-09 20:18:50 +0300

    Added heatmap for HV,TH (doesn't working) Also added HistogramMaker based on Muon_data Added Histogram data csv made by the histogram maker script.

---
+++ Heatmap.py
@@ -0,0 +1,17 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import csv
+import scipy.interpolate
+from matplotlib import cm
+datafile=open('C:\\Users\\Ohad\\\Desktop\\muon_calibration_II.csv','r')
+data = np.array(list(csv.reader(datafile)))
+new_data=data[1:,1:] #without the titles line and the index column
+th,hv,n_5=new_data[:,:1],new_data[:,1:2],new_data[:,-1] #x contains TH,HV , y contains N[5min]
+x = (np.squeeze(np.asarray(th)).astype(float))
+y = (np.squeeze(np.asarray(hv)).astype(float))
+z = (np.squeeze(np.asarray(n_5)).astype(float))
+plt.scatter(x,y,z)
+plt.gray()
+plt.show()
+
---
+++ HistogramMaker.py
@@ -0,0 +1,36 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import csv
+import scipy.interpolate
+from matplotlib import cm
+
+THRESHOLD=40000.0
+datafile=open('C:\\Users\\Ohad\\\Desktop\\muon_data_II.csv','r')
+data = np.array(list(csv.reader(datafile)))
+new_data=data[1:,1:2] #only the lifetimes column
+lifetimes=(np.squeeze(np.asarray(new_data)).astype(float)).ravel()
+good_lifetimes=[]
+for lifetime in lifetimes:
+    if lifetime<THRESHOLD:
+        good_lifetimes.append(lifetime)
+good_lifetimes=np.array(good_lifetimes)
+print(good_lifetimes.max())
+histo,bin_edges=np.histogram(good_lifetimes,bins=45)
+f=plt.figure(1)
+plt.hist(good_lifetimes,bins=45)
+f.show()
+s=plt.figure(2)
+plt.hist(good_lifetimes,bins=60)
+s.show()
+t=plt.figure(3)
+plt.hist(good_lifetimes,bins=100)
+t.show()
+"""""
+rows=zip(histo,bin_edges)
+with open('HistoResults.csv', 'w', ) as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    for row in rows:
+        wr.writerow(row)
+myfile.close()
+"""
